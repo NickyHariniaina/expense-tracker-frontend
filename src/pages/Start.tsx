4549d8d77fd18dda7ae0c177136e348bfd58a60f
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-09-04 17:52:09 +0000

    feat : add particle backgound using particle js typescript

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,23 +1,27 @@
 import Button from "../components/Button/Button";
 import Logo from "../components/Logo/Logo";
+import ParticleBackground from "../components/ParticuleBackground";
 
 export default function Start() {
   return (
     <div className="w-screen h-screen overflow-hidden relative">
-      <h1 className="text-[32px] font-bold m-4 absolute top-0 left-0">
+
+      <ParticleBackground />
+      <h1 className="font-baskerville  text-[24px] font-bold m-4 absolute top-0 left-0 z-10">
         WalletWatch
       </h1>
 
-      <div className="w-full h-full flex flex-col items-center justify-center text-center space-y-4">
+      <div className="w-full h-full flex flex-col items-center justify-center text-center space-y-4 relative z-10">
       <Logo size={150} />
-        <h2 className="text-[44px] font-bold w-1/2">
+        <h2 className="text-[50px] font-bold w-1/2 font-baskerville">
           Take a full control of your money - all in one place
         </h2>
-        <h3 className="text-[16px]">
+        <h3 className="text-[16px] font-normal">
           Your money, Your plan, Your growth
         </h3>
         <Button text="GET STARTED" bg_color="secondary" size="lg" />
       </div>
+
     </div>
   );
 }
