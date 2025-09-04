9f566b8251bf03c5785f15097b675184959d89cb
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-09-04 21:07:05 +0000

    init : add LoginForm and SignupForm using to be components

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,6 +1,8 @@
-function Auth () {
+import React from "react";
+
+const Auth: React.FC = () => {
   return (
-    <div>
+    <div className="w-full h-full flex text-center space-y-4 relative z-10">
       <h1>Authentication Page</h1>
       <p>Welcome to the auth page!</p>
     </div>
