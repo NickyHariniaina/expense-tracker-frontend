c4e63fb78a70ca91c83e9cddd932f334168f6cdb
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-05 16:23:03 +0300

    refactor(dashboard): add better log for debug

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,18 +1,22 @@
-import { useEffect, type FC } from "react";
+import { type FC } from "react";
 import SideBar from "../components/SideBar/Sidebar";
 import { useUserStore } from "../store/user";
-import { login } from "../utils/auth";
+import { useFetchBasicUser } from "../hooks/useFetchBasicData";
 
 const DashBoard: FC = () => {
-  const { userData, fetchUserData } = useUserStore();
+  const { userData, userCategories, userExpenses, userIncomes, userSummary } =
+    useUserStore();
 
-  // REMOVE THIS LATER, WHEN THE LOGIN PAGE IS WORKING
+  useFetchBasicUser();
 
-  useEffect(() => {
-    login("nicky@mail.hei.school", "12345678");
-    fetchUserData();
-  }, [fetchUserData]);
-      console.log(userData);
+  // JUST FOR VISUAL HELP
+  console.group("User Data");
+  console.log("UserData:", userData);
+  console.log("Categories:", userCategories);
+  console.log("Expenses:", userExpenses);
+  console.log("Incomes:", userIncomes);
+  console.log("Summary:", userSummary);
+  console.groupEnd();
 
   return (
     <>
--- src/utils/user.ts
+++
@@ -1,7 +0,0 @@
-import { useUserStore } from "../store/user"
-
-export const fetchBasicUserData = () => {
-  try {
-    const {fetchUserData, } = useUserStore()
-  }
-}
