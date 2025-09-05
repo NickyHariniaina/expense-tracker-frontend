0a90838e43b5b06360ba6a16c4da8fb898b35e91
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-05 16:01:43 +0300

    feat: add principal fetch store

--- src/store/user.ts
+++ src/store/user.ts
@@ -19,7 +19,7 @@ interface UserState {
 // BASE_URL, should be put in the env later
 const BASE_URL: string = "http://localhost:3000/api";
 
-export const useUserStore = create<UserState>((set) => ({
+export const useUserStore = create<UserState>((set, get) => ({
   userData: null,
   userCategories: null,
   userExpenses: null,
@@ -45,6 +45,101 @@ export const useUserStore = create<UserState>((set) => ({
       }
 
       set({ userData: data });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  fecthUserCategories: async () => {
+    try {
+      const response = await fetch(BASE_URL + "/categories", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+
+      if (response.status !== 200) {
+        throw new Error("An error ocurred with the server");
+      }
+
+      set({
+        userCategories: data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  fetchUserExpenses: async () => {
+    try {
+      const response = await fetch(BASE_URL + "/expenses", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+
+      if (response.status !== 200) {
+        throw new Error("An error ocurred with the server");
+      }
+
+      set({
+        userExpenses: data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  fetchUserIncomes: async () => {
+    try {
+      const response = await fetch(BASE_URL + "/incomes", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+
+      if (response.status !== 200) {
+        throw new Error("An error ocurred with the server");
+      }
+
+      set({
+        userIncomes: data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  fetchUserSummary: async () => {
+    try {
+      const startDate = get().userData?.start_date;
+      const response = await fetch(BASE_URL + "/summary?start=" + startDate, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+
+      if (response.status !== 200) {
+        throw new Error("An error ocurred with the server");
+      }
+
+      set({
+        userSummary: data,
+      });
     } catch (error) {
       console.log(error);
     }
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,4 @@
-export const login = async (email, password) => {
+export const login = async (email: string, password: string) => {
   try {
     const res = await fetch("http://localhost:3000/api/auth/login", {
       method: "POST",
