0e8005379c871a2bc2e3deae74cf7671615acc29
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-05 17:06:25 +0300

    feat(incomes.ts): add update incomes.

--- src/utils/incomes.ts
+++ src/utils/incomes.ts
@@ -23,3 +23,29 @@ export const createIncome = async (
     console.log(error);
   }
 };
+
+export const updateIncome = async (
+  amount: number,
+  date: Date,
+  source: string,
+  description: string | null,
+) => {
+  try {
+    const res = await fetch("http://localhost:3000/api/incomes", {
+      method: "PUT",
+      body: JSON.stringify({
+        amount: amount,
+        date: date,
+        source: source,
+        description: description,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    // CAN YOU PLEASE VERIFY EACH CASE WITH res.status ?
+  } catch (error) {
+    console.log(error);
+  }
+};
