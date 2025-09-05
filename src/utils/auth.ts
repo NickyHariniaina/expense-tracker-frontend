43eebf361bd1c0c9cc9e5b67ee1c77b8254737eb
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-05 16:32:03 +0300

    feat(auth.ts): add signup function

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -13,6 +13,34 @@ export const login = async (email: string, password: string) => {
     });
     if (res.status === 200) {
       console.log("logged in");
+    } else if (res.status === 404) {
+      console.log("User not found");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = async (email: string, password: string) => {
+  try {
+    const res = await fetch("http://localhost:3000/api/auth/signup", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+
+    if (res.status === 200) {
+      console.log("user created");
+    } else if (res.status === 409) {
+      console.log("User already exist");
+    } else if (res.status === 500) {
+      console.log("Server Error");
     }
   } catch (error) {
     console.log(error);
