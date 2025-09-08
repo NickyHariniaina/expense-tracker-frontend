2fff5bbc9f86f47671e3ffee812b4d5366b228d4
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-08 11:06:58 +0300

    feat: error handling in summary

--- src/utils/summary.ts
+++ src/utils/summary.ts
@@ -1,3 +1,4 @@
+import toast from "react-hot-toast";
 export const getMonthlySummary = async (date: Date) => {
   try {
     const res = await fetch(
@@ -7,8 +8,18 @@ export const getMonthlySummary = async (date: Date) => {
         credentials: "include",
       },
     );
-    const data = await res.json();
-    return data;
+    if (res.status === 200) {
+      toast.success("Monthly summary retrieved 📅");
+
+      const data = await res.json();
+      return data;
+    } else if (res.status === 401) {
+      toast.error("Unauthorized 🚫");
+    } else if (res.status === 500) {
+      toast.error("Internal Server Error 💥");
+    } else {
+      toast.error(`Unexpected status: ${res.status}`);
+    }
   } catch (error) {
     console.log(error);
   }
@@ -21,7 +32,7 @@ export const getAlert = async () => {
       credentials: "include",
     });
     const data = await res.json();
-    return data;
+    toast.success(data.message);
   } catch (error) {
     console.log(error);
   }
