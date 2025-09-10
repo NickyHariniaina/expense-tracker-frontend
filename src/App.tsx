035f1656a507992ed6620468fbafafb353a32e36
Zinedis <nantenainabakari@gmail.com>
2025-09-10 17:06:33 +0200

    Feat change App.tsx for test

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,19 @@ import { Toaster } from "react-hot-toast";
 
 const App: React.FC = () => {
   return (
-    <>
-    
     <Router>
-      <Routes>
-        <Route path="/" element={<Start />} />
-        <Route path="/Auth" element={<Auth />} />
-        <Route path="/dashboard" element={<DashBoard />} />
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/expense" element={<h1>Expense Page</h1>} />
+          <Route path="/income" element={<h1>Income Page</h1>} />
+          <Route path="/summary" element={<h1>Summary Page</h1>} />
+        </Routes>
         <Toaster />
-      </Routes>
     </Router>
-  
-    </>
+    
   );
 };
+
 export default App;
