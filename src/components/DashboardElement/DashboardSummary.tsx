e9b91598acb4e852dff11ce1d47d87d08676f968
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-09-10 20:48:43 +0000

    feat : init chart-js-2 and add all function that DashboardSummary need

--- src/components/DashboardElement/DashboardSummary.tsx
+++ src/components/DashboardElement/DashboardSummary.tsx
@@ -1,6 +1,44 @@
 import type React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { useState } from "react";
+
+// Save components on ChartJS
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface MonthlySummary {
+  totalIncome : number;
+  totalExpense : number;
+  balance : number;
+  expenseByCategory : { [key: string]: number };
+  monthlyTrend : { month: string; income: number; expense: number }[];
+}
 
 const DashboardSummary: React.FC = () => {
+  const [summary, setSummary] = useState<MonthlySummary | null> (null);
+  const [loading, setLoading] = useState(true);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
     <>
       <h1>Hello Summary</h1>
