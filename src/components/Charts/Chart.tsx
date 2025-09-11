bc93cd1c49c29810fdc3e934e15f988bbed6f847
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-12 02:38:39 +0300

    refactor: remove type any

--- src/components/Charts/Chart.tsx
+++ src/components/Charts/Chart.tsx
@@ -2,7 +2,6 @@ import type React from "react";
 import { Line } from "react-chartjs-2";
 import { getChartConfig } from "../../utils/chartConfig";
 import { useState, useEffect } from "react";
-import { data } from "react-router-dom";
 import Loading from "../Loading/Loading";
 
 interface ChartProps {
@@ -11,8 +10,8 @@ interface ChartProps {
 
 const Chart: React.FC<ChartProps> = ({ currentYear }) => {
   const [chartConfig, setChartConfig] = useState<{
-    data: any;
-    options: any;
+    data;
+    options;
   } | null>(null);
 
   useEffect(() => {
