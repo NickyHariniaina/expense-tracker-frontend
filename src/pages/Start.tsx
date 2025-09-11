b08cbc37be99d714ec5ceb2c10ae82976fa704b0
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-11 14:21:51 +0300

    feat(Start.tsx): add user verification

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,14 +1,23 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button/Button";
 import Logo from "../components/Logo/Logo";
 import ParticleBackground from "../components/ParticuleBackground";
-import Banner from "../components/Banner/Banner";
+import { useUserStore } from "../store/user";
+import { useFetchBasicUser } from "../hooks/useFetchBasicData";
 
 export default function Start() {
-  {
-    /**redirection to auth page */
-  }
   const navigate = useNavigate();
+  const { userData } = useUserStore();
+
+  useFetchBasicUser();
+
+  useEffect(() => {
+    if (userData) {
+      navigate("/dashboard");
+    }
+  }, [userData, navigate]);
+
   const handleClick = () => {
     navigate("/auth");
   };
@@ -16,21 +25,19 @@ export default function Start() {
   return (
     <div className="w-screen h-screen overflow-hidden relative">
       <ParticleBackground />
-      <h1 className="font-baskerville  text-[24px] font-bold m-4 absolute top-0 left-0 z-10">
+      <h1 className="font-baskerville text-[24px] font-bold m-4 absolute top-0 left-0 z-10">
         WalletWatch
       </h1>
 
-      {/**All element in the center */}
       <div className="w-full h-full flex flex-col items-center justify-center text-center space-y-4 relative z-10">
         <Logo size={150} />
-        <h2 className="text-[50px] font-bold w-1/2 font-baskerville text-">
-          Take a full control of your money - all in one place
+        <h2 className="text-[50px] font-bold w-1/2 font-baskerville">
+          Take full control of your money – all in one place
         </h2>
         <h3 className="text-[24px] font-spartan font-light py-3">
           Your money, Your plan, Your growth
         </h3>
 
-        {/*Button to go to the login page*/}
         <Button
           onClick={handleClick}
           text="GET STARTED"
@@ -39,9 +46,6 @@ export default function Start() {
           className="hover:bg-[#059669]! transition-all! duration-300 ease-in-out"
         />
       </div>
-
-      {/**banner */}
-      <Banner />
     </div>
   );
 }
